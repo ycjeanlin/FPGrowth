f46cfbcdb74d0eb51351876213bd639878bc6ec7
Jean <Jean@Jean-PC>
2014-10-06 20:16:09 +0800

    Final version

--- exp_sup.bat
+++ exp_sup.bat
@@ -1,6 +1,5 @@
-java -jar exp.jar -fs 0.1 -ts 0.5 -fc 50 -tc 50 -d 0.1 -o s -db D1kT10N500.txt
-java -jar exp.jar -fs 0.1 -ts 0.5 -fc 50 -tc 50 -d 0.1 -o s -db D10kT10N1k.txt
 java -jar exp.jar -fs 0.1 -ts 0.5 -fc 50 -tc 50 -d 0.1 -o s -db D100kT10N1k.txt
+java -jar exp.jar -fs 0.1 -ts 0.5 -fc 50 -tc 50 -d 0.1 -o s -db D10kT10N1k.txt
 java -jar exp.jar -fs 10 -ts 50 -fc 50 -tc 50 -d 10 -o s -db Mushroom.txt
-
+java -jar exp.jar -fs 0.1 -ts 0.5 -fc 50 -tc 50 -d 0.1 -o s -db D1kT10N500.txt
 cmd
